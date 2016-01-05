0bacdda0709ac17b2c4201bdee6df77d9b21dbd8
Ivan Masmitja <imasmitja@gmail.com>
2016-01-05 13:58:40 +0100

    Extract FileFormatSerieC

--- Source/SamsungChanelEditor/Samsung/Definitions/FileFormatSerieC.cs
+++ Source/SamsungChanelEditor/Samsung/Definitions/FileFormatSerieC.cs
@@ -1,6 +1,4 @@
-﻿using SamsChannelEditor.Samsung.Definitions;
-
-namespace SamsChannelEditor.Samsung
+﻿namespace SamsChannelEditor.Samsung.Definitions
 {
     internal class FileFormatSerieC : FileFormat
     {
@@ -8,19 +6,6 @@ namespace SamsChannelEditor.Samsung
         {
             Favorites = 4;
             MapAirD = new ChannelFormat292();
-        }
-    }
-
-
-    internal abstract class FileFormat
-    {
-        public string Id { get; protected set; }
-        public int Favorites { get; protected set; }
-        public ChannelFormat MapAirD { get; protected set; }
-
-        protected FileFormat(string id)
-        {
-            Id = id;
         }
     }
 }
